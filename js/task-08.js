4a3c298441c0c2d1782b1d918314176bbdc5d8dc
vikroriiakovalova <vika.shandruk13@gmail.com>
2021-09-23 09:43:42 +0300

    финализирует решение 7 задач

---
+++ js/task-08.js
@@ -0,0 +1,20 @@
+// const refs = {
+//     wrapperEl: document.querySelector('#boxes'),
+//     inputEl: document.querySelector('input'),
+//     renderBtnEl: document.querySelector('[data-action="render"]'),
+//     destroyBtnEl: document.querySelector('[data-action="destroy"]'),
+// };
+
+// refs.renderBtnEl.addEventListener('click', () => {
+//     const createBoxes = (amount) => {
+//     if (amount === number) {
+//         amount.forEach(element => {
+//             const boxEl = document.createElement('div');
+//             boxEl[0].style.width = '30px';
+//             boxEl[0].style.height = '30px';
+//             console.log(boxEl);
+//         });
+//     }
+//     };
+    
+// })
